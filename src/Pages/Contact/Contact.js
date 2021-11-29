9ade76de4120420022f628431562e5990a847d8c
samimeadad <sami.meadad@gmail.com>
2021-11-29 23:24:29 +0600

    email send done

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+import { Button, Form } from 'react-bootstrap';
 
 const Contact = () => {
+    const sendEmail = ( e ) => {
+        e.preventDefault();
+        emailjs.sendForm( 'service_wkovuj4', 'template_0bj3nlo', e.target, 'user_NTt1KPYzFQZfll2DEFxve' )
+            .then( ( result ) => {
+                console.log( result.text );
+            }, ( error ) => {
+                console.log( error.text );
+            } );
+        e.target.reset();
+    };
     return (
-        <div>
-            <h2>Contact Page</h2>
-        </div>
+        <section className="my-5">
+            <h1 className="my-5 text-primary">Please Write to Me</h1>
+            <Form onSubmit={ sendEmail } className="form w-50 mx-auto">
+                <Form.Group className="mb-3" controlId="formBasicSubject">
+                    <Form.Control type="text" placeholder="Subject" name='subject' />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Control type="text" placeholder="Your Name" name='name' />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter Your Email" name='email' />
+                    <Form.Text className="text-muted">
+                        I'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Your Message</Form.Label>
+                    <Form.Control as="textarea" rows={ 5 } name='message' />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Send Mail
+                </Button>
+            </Form>
+        </section >
     );
 };
 
